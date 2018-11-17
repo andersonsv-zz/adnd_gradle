c5f84af9447a15fa700b6d1abfc7d4ab24821bea
Your Name <anderson.si.vieira@hotmail.com>
2018-11-17 16:28:48 -0200

    Endpoint chamando gerador de piadas

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyBean.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyBean.java
@@ -1,15 +1,14 @@
 package com.udacity.gradle.builditbigger.backend;
 
+import andersonsv.com.br.jokelib.JokeGen;
+
 /** The object model for the data we are sending through endpoints */
 public class MyBean {
 
     private String myData;
 
     public String getData() {
+        myData = new JokeGen().getRandomJoke();
         return myData;
-    }
-
-    public void setData(String data) {
-        myData = data;
     }
 }
